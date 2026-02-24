ab03753f731213e2d5b33d667c60783a8d653a5d
Denixon Bravo <119322894+denixon13@users.noreply.github.com>
2026-02-23 21:03:49 -0400

    configuracion de base de datos

--- vestier/settings.py
+++ vestier/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,14 +75,11 @@ WSGI_APPLICATION = 'vestier.wsgi.application'
 
 # Database
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'vestierdb',
-        'USER': 'postgres',
-        'PASSWORD': 'admin',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.config(
+        default='postgresql://postgres:admin@localhost:5432/vestierdb',
+        conn_max_age=600,
+        ssl_require=False if 'localhost' in os.environ.get('DATABASE_URL', '') else True
+    )
 }
 
 # Internationalization
